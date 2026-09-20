e6b869d857d90b196f411cdfd1c07e82f1593ae1
lakshyasainiprimary-netizen <lakshya.saini.primary@gmail.com>
2026-09-20 20:16:00 +0530

    Update LINEAR_SEARCH.cpp

--- ARRAYS/LINEAR_SEARCH.cpp
+++ ARRAYS/LINEAR_SEARCH.cpp
@@ -14,21 +14,21 @@ using namespace std;
 
 int LinearSearch(vector<int> &array, int target) {
 
-    for(int i = 0; i < array.size(); i++) {
-        if(array[i] == target) {
+    for(int i = 0; i < array.size(); i++) {//SCANS THE ARRAY :
+        if(array[i] == target) {//TARGET FOUND :
 
-            return i;
+            return i;//RETURN INDEX OF THE TARGET :
 
         }
     }
 
-        return -1;
+        return -1;//IF TARGET NOT FOUND :
 }
 
 int main(){
 
     vector<int> array = {4, 7, 2, 9, 1, 6};
 
-    int target = 9;
-    cout << LinearSearch(array, target) << endl;
+    int target = 9;//TARGET DECLARATION FOR THIS PROBLEM, IN GENERAL ASK FOR USER INPUT FOR A RANDOM TARGET :
+    cout << LinearSearch(array, target) << endl;//FUNCTION CALLING :
 }
